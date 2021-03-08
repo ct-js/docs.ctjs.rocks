9909396acc8a14b4d423bf1833e638ceacd653c8
Cosmo Myzrail Gorynych <admin@nersta.ru>
2021-03-08 21:39:34 +1200

    :pencil: Add "Dragging Copies Around" tutorial by @qewer33

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -110,6 +110,7 @@ module.exports = {
                     'localstorage',
                     'viewport-management',
                     'textures-magic-properties',
+                    'dragging-copies',
                     'gamedev-resources'
                 ]
             }, {
